15392bae9afba98cc9b92a03b0214b09d09be7d6
Bhavya <bhavyadoriya2007@gmail.com>
2026-03-14 17:07:25 +0530

    core_api/core_api/settings.py changed

--- core_api/core_api/settings.py
+++ core_api/core_api/settings.py
@@ -11,15 +11,6 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-<<<<<<< HEAD
-
-=======
-import os
-import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv()
->>>>>>> 81ef4baaff35d9fe16ea7ec1e8fc350e822394c3
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -94,7 +85,6 @@ WSGI_APPLICATION = 'core_api.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-<<<<<<< HEAD
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
@@ -102,15 +92,6 @@ DATABASES = {
 }
 
 
-=======
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
-
->>>>>>> 81ef4baaff35d9fe16ea7ec1e8fc350e822394c3
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
